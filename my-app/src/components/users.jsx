719cede80475a5e2d210507a23f751f7b33b2b14
Anatoly Fomin <58336508+Fomin-Anatolii@users.noreply.github.com>
2022-07-07 17:51:07 +0800

    created fast-company

---
+++ my-app/src/components/users.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from "react"
+import api from "../api"
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll())
+
+  const handleDelete = (userId) => {
+    setUsers((prevState) => prevState.filter((name) => name !== userId))
+  }
+
+  const handlePhrase = (number) => {
+    number = users.length
+    let people = `человек`
+    let word = `тусанёт`
+
+    if (number === 3 || number === 2 || number === 4) {
+      word = `тусанут`
+      people = `человека`
+    }
+
+    const getClasses = () => {
+      let classes = "badge"
+      number !== 0 ? (classes += ` bg-primary`) : (classes += " bg-danger")
+      return classes
+    }
+
+    let span = (
+      <span className={getClasses()}>
+        {number} {people} {word} с тобой сегодня
+      </span>
+    )
+
+    return span
+  }
+
+  const headRow = () => {
+    const headArr = [`Имя`, `Качества`, `Профессия`, `Встретился, раз`, `Оценка`]
+    return headArr.map((title) => <th key={title}>{title}</th>)
+  }
+
+  const getBadges = (qualities) => {
+    return qualities.map(({ _id, name, color }) => (
+      <span className={"badge  m-1 bg-" + color} key={_id}>
+        {name}
+      </span>
+    ))
+  }
+
+  const bodyRow = () => {
+    return users.map((user) => (
+      <tr key={user._id}>
+        <td>{user.name}</td>
+        <td>{getBadges(user.qualities)}</td>
+        <td>{user.profession.name}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate}/5</td>
+        <td>
+          <button
+            type="button"
+            className="btn btn-danger btn-sm"
+            onClick={() => handleDelete(user)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ))
+  }
+
+  return (
+    <>
+      <h2>{handlePhrase()}</h2>
+      <table className="table table-hover">
+        <thead>
+          <tr>{headRow()}</tr>
+        </thead>
+        <tbody>{bodyRow()}</tbody>
+      </table>
+    </>
+  )
+}
+
+export default Users
