f86d584c81029dae0e19a754d3fe998f5a9b9373
Anatoly Fomin <58336508+Fomin-Anatolii@users.noreply.github.com>
2022-07-17 22:11:43 +0800

    changed className in favorites

--- my-app/src/app/components/bookmark.jsx
+++ my-app/src/app/components/bookmark.jsx
@@ -1,7 +1,7 @@
 const Bookmark = ({ favorites, ...rest }) => {
   return (
     <button onClick={() => rest.handleToggleBookmark(rest.id)}>
-      <i className={favorites ? "bi bi-bookmark" : "bi bi-bookmark-fill"}></i>
+      <i className={favorites ? "bi bi-bookmark-fill" : "bi bi-bookmark"}></i>
     </button>
   )
 }
