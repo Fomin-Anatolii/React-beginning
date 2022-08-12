5d17ac8627e143a02596e9aa30fe0f2a9ca2cd6b
Anatoly Fomin <58336508+Fomin-Anatolii@users.noreply.github.com>
2022-08-12 21:32:55 +0800

    created tableBody

--- fast-company/src/app/components/usersTable.jsx
+++ fast-company/src/app/components/usersTable.jsx
@@ -1,31 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
-import User from "./user"
 import TableHeader from "./tableHeader"
+import TableBody from "./tableBody"
 
 const UserTable = ({ users, onSort, selectedSort, ...rest }) => {
     const columns = {
-        name: { iter: "name", name: "Имя" },
+        name: { path: "name", name: "Имя" },
         qualities: { name: "Качества" },
-        professions: { iter: "profession.name", name: "Профессия" },
+        professions: { path: "profession.name", name: "Профессия" },
         completedMeetings: {
-            iter: "completedMeetings",
+            path: "completedMeetings",
             name: "Встретился, раз"
         },
-        rate: { iter: "rate", name: "Оценка" },
-        bookmark: { iter: "bookmark", name: "Избранное" },
+        rate: { path: "rate", name: "Оценка" },
+        bookmark: { path: "bookmark", name: "Избранное" },
         delete: {}
     }
     return (
         <table className="table">
             <TableHeader {...{ onSort, selectedSort, columns }} />
-            <tbody>
+            <TableBody {...{ columns, data: users }} />
+            {/* <tbody>
                 {users.map((user) => (
                     <tr key={user._id}>
                         <User {...user} {...rest} />
                     </tr>
                 ))}
-            </tbody>
+            </tbody> */}
         </table>
     )
 }
