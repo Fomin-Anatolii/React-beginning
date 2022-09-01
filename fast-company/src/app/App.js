afa1073ed6b903cde7b50802034e4a8abf5d3424
Anatoly Fomin <58336508+Fomin-Anatolii@users.noreply.github.com>
2022-09-01 09:55:08 +0800

    added Redirect in App

--- fast-company/src/app/App.js
+++ fast-company/src/app/App.js
@@ -1,21 +1,22 @@
 import React from "react"
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import Users from "./components/layouts/users.jsx"
 import Main from "./components/layouts/main"
 import Login from "./components/layouts/login.jsx"
 import NavBar from "./components/navBar"
 
 function App() {
-    return (
-        <div>
-            <NavBar />
-            <Switch>
-                <Route exact path="/" component={Main} />
-                <Route path="/login" component={Login} />
-                <Route path="/users/:usersID?" component={Users} />
-            </Switch>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/login" component={Login} />
+        <Route path="/users/:usersID?" component={Users} />
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
